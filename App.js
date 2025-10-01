a0d0a01a33bbc4eeaee8deef3b80b7e1638e1ce4
deepu filius <deepuadari@gmail.com>
2025-10-01 20:10:59 +0530

    add React CDN links

--- App.js
+++ App.js
@@ -5,3 +5,9 @@ h2.innerText = "Hello World from JavaScript";
 const rootElement = document.getElementById("root");
 
 rootElement.appendChild(h2);
+
+const h3 = React.createElement("h3",{id:"section"},"Hello World from React CDN");
+
+const root = ReactDOM.createRoot(document.getElementById("root2"));
+
+root.render(h3);
